1e9eea8c680bca42b2db69ac09ba581f366b9b1c
Dmitry Antonenko <d.antonenko@rdteam.ru>
2023-09-24 12:08:07 +0300

    fix changes

--- app/services/logos/creates/filter.rb
+++ app/services/logos/creates/filter.rb
@@ -12,7 +12,7 @@ module Logos
       end
 
       def call
-        by_company_name
+        # by_company_name
         by_slogan
         by_icon
         by_industry
@@ -23,9 +23,13 @@ module Logos
       private
 
       def by_company_name
-        self.templates = templates.joins(:company_leng_datas)
-                                  .where({ company_length_datas: { 'company_name_length_min >= ? ' => company_name.size } })
-                                  .where({ company_length_datas: { 'company_name_length_max <= ? ' => company_name.size } })
+        # self.templates = templates.joins(:company_length_datas)
+        #                          .where({ company_length_datas: { 'company_name_length_min >= ? ' => company_name.size } })
+        #                          .where({ company_length_datas: { 'company_name_length_max <= ? ' => company_name.size } })
+
+        self.templates = templates.joins(:company_length_datas)
+                                  .where('company_length_datas.company_name_length_min >= ?' => company_name.size)
+                                  .where('company_length_datas.company_name_length_max <= ?' => company_name.size)
       end
 
       def by_slogan
@@ -33,12 +37,12 @@ module Logos
       end
 
       def by_icon
-        self.templates = templates.where(icon_required: true) unless icon_ids.empty?
+        self.templates = templates.where(icon_required: true) if icon_ids && !icon_ids.empty?
       end
 
       def by_industry
         self.templates = templates.joins(:advanced_datas)
-                                  .where(advanced_datas: { industry_id: industry_id })
+                                  .where(advanced_datas: { industry_id: industry_id }) if industry_id
       end
     end
   end
--- spec/services/logos/creates/filter_spec.rb
+++ spec/services/logos/creates/filter_spec.rb
@@ -1,12 +1,50 @@
 require 'rails_helper'
 RSpec.describe Logos::Creates::Filter, type: :service do
 
+  describe 'filter by company name' do
+    let!(:short_company_name_template) { create(:template) }
+    let!(:company_length_data) { create(:company_length_data,
+                                        template_id: short_company_name_template.id,
+                                        company_name_length_min: 5,
+                                        company_name_length_max: 7) }
 
-  describe 'test1' do
-    context 'test2' do
-      it 'test3' do
-        1
-      end
+    let!(:long_company_name_template) { create(:template) }
+    let!(:company_length_data) { create(:company_length_data,
+                                        template_id: short_company_name_template.id,
+                                        company_name_length_min: 10,
+                                        company_name_length_max: 12) }
+
+    it 'short company must be result in filer by short companies' do
+      res =  Logos::Creates::Filter.new(Template.all, '123456', nil, nil, []).call
+
+      expect(res.size).to eq(1)
+      expect(res.firt.id).to eq(short_company_name_template.id)
+    end
+
+    it 'long company must be result in filter by long companies' do
+      res =  Logos::Creates::Filter.new(Template.all, '12345678901', nil, nil, []).call
+
+      expect(res.size).to eq(1)
+      expect(res.firt.id).to eq(long_company_name_template.id)
+    end
+  end
+
+  describe 'filter by slogan' do
+    let!(:template_with_slogan) { create(:template, slogan_required: true) }
+    let!(:template_without_slogan) { create(:template, slogan_required: false) }
+
+
+    it 'Template with slogan required must be return' do
+      res =  Logos::Creates::Filter.new(Template.all, '123456', nil, 'slogan', []).call
+
+      expect(res.size).to eq(1)
+      expect(res.first.id).to eq(template_with_slogan.id)
+    end
+
+    it 'Template with slogan required must be return' do
+      res =  Logos::Creates::Filter.new(Template.all, '12345678901', nil, nil, []).call
+
+      expect(res.size).to eq(2)
     end
   end
 end
